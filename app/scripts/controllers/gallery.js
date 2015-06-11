0a4f2ecc501de9f12f92b34de2994c0da293a229
Artem Puchenkin <apuchenkin@gmail.com>
2015-06-11 12:55:17 +0300

    use dynamic thumbnails

--- app/scripts/controllers/gallery.js
+++ app/scripts/controllers/gallery.js
@@ -10,52 +10,54 @@
 angular.module('aws.photo.client')
   .controller('aws.controller.gallery', ['$timeout', 'photos', 'CONFIG',
     function ($timeout, photos, config) {
-      var me = this;
+      var me = this,
+          sha = new Hashes.SHA1(),
+          views = _.pluck(photos, 'views'),
+          std = math.std(views),
+          max = math.max(views),
+          gutter = me.gutter = config.gutter,
+          s = me.size = config.galleryColumnWidth,
+          s2 = (s * 2) + gutter,
+          s3 = (s * 3) + (gutter * 2),
+          s4 = (s * 4) + (me.gutter * 3)
+      ;
+
       me.bricks = [];
-      me.gutter = config.gutter;
-
-      var views = _.pluck(photos, 'views');
-      var std = math.std(views);
-      var max = math.max(views);
-
-      var s = me.size = config.galleryColumnWidth;
-      var s2 = (s * 2) + me.gutter;
-      var s3 = (s * 3) + (me.gutter * 2);
-      var s4 = (s * 4) + (me.gutter * 3);
 
       var getBrick = function (photo) {
-        var div = Math.floor(photo.views / (std + 1)) + 1;
-        var agg = Math.floor(max / ((photo.views * std) + 1)) + 1;
+        var div = Math.floor(photo.views / (std + 1)) + 1,
+            agg = Math.floor(max / ((photo.views * std) + 1)) + 1,
+            probs, mode, isHorisontal, ratio, dims, dsmap, sign, msize;
 
-        var probs = _.flatten([
+        probs = _.flatten([
           _.fill(new Array((agg * agg * 8) + (div * div * 1)), 1),
           _.fill(new Array((agg * agg * 4) + (div * div * 2)), 2),
           _.fill(new Array((agg * agg * 2) + (div * div * 3)), 3),
           _.fill(new Array((agg * agg * 1) + (div * div * 4)), 4)
         ]);
 
-        var mode = _.sample(probs),
-          isHorisontal = (photo.width > photo.height),
-          pk = Math.floor(photo.width / photo.height),
-          dims, dsmap;
+        mode = _.sample(probs);
+        isHorisontal = (photo.width > photo.height);
+        ratio = Math.floor(photo.width / photo.height);
 
         dsmap = {
-          1: [pk >= 2 ? s2 : s, s],
-          2: isHorisontal ? [pk >= 3 ? s3 : s2, s] : [s, s2],
-          3: pk >= 4 ? [s4, s] : [pk >= 2 ? s3 : s2, s2],
-          4: isHorisontal ? [pk >= 2 ? s4 : s3, s2] : [s2, s3]
+          1: [ratio >= 2 ? s2 : s, s],
+          2: isHorisontal ? [ratio >= 3 ? s3 : s2, s] : [s, s2],
+          3: ratio >= 4 ? [s4, s] : [ratio >= 2 ? s3 : s2, s2],
+          4: isHorisontal ? [ratio >= 2 ? s4 : s3, s2] : [s2, s3]
         };
 
         dims = dsmap[mode];
+        msize = _.max([dims[0], dims[1]]);
+        sign = sha.hex_hmac(config.secret, photo.id + "-" + msize + 'x' + msize);
 
         return {
           id: photo.id,
           width: dims[0],
           height: dims[1],
-          src: config.staticEndpoint + photo.thumb
+          src: [config.apiEndpoint, 'hs/photo', photo.id, msize, msize, sign].join('/')
         };
       };
-
 
       angular.extend(me.bricks, photos.map(getBrick));
 
